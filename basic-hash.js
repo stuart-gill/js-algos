0112766804d360ec4f72aedd3c92e08704dd1405
Stuart Gill <stuart.a.gill@gmail.com>
2020-03-12 12:13:20 -0700

    define working setter and getter for basic hash table

--- basic-hash.js
+++ basic-hash.js
@@ -13,7 +13,34 @@ class HashTable {
     }
     return total;
   }
+
+  set(key, value) {
+    let selectedElement = this.keyMap[this._hash(key)];
+    if (selectedElement) {
+      selectedElement.push([key, value]);
+    } else {
+      // if selectedElement is undefined, we must create the array that will take this spot
+      this.keyMap[this._hash(key)] = [[key, value]];
+    }
+  }
+
+  get(key) {
+    let selectedElement = this.keyMap[this._hash(key)];
+    if (!selectedElement) return undefined;
+    if (selectedElement.length > 1) {
+      for (let e of selectedElement) {
+        if (e[0] === key) return [key, value];
+      }
+    } else if (selectedElement.length === 1) {
+      return selectedElement[0];
+    } else return undefined;
+  }
 }
 
 let ht = new HashTable();
-console.log(ht._hash('my name is'));
+
+console.log(ht.set('pink', 123));
+console.log(ht.set('pred', 123));
+console.log(ht.set('nep', 123));
+console.log(ht.set('donker', 123));
+console.log(ht.get('pink'));
