b44ad62f31038cda46944d387638009583e0dfae
Stuart Gill <stuart.a.gill@gmail.com>
2020-03-16 20:03:44 -0700

    use efficient priority queue rather than array sort in weighted graph

--- weighted-graph.js
+++ weighted-graph.js
@@ -23,8 +23,6 @@ class WeightedGraph {
       priorityQueue.enqueue(vertex, distances[vertex]);
     }
     let currentVertex;
-    // console.log(distances);
-    console.log(priorityQueue);
 
     while (priorityQueue.values.length > 0) {
       currentVertex = priorityQueue.dequeue().val;
@@ -39,6 +37,7 @@ class WeightedGraph {
         console.log(distances);
         break;
       }
+
       for (const neighbor of adjacencyList[currentVertex]) {
         console.log(neighbor);
         let candidateWeight = distances[currentVertex] + neighbor.weight;
@@ -59,19 +58,89 @@ class WeightedGraph {
   }
 }
 
+// class PriorityQueue {
+//   constructor() {
+//     this.values = [];
+//   }
+//   enqueue(val, priority) {
+//     this.values.push({ val, priority });
+//     this.sort();
+//   }
+//   dequeue() {
+//     return this.values.shift();
+//   }
+//   sort() {
+//     this.values.sort((a, b) => a.priority - b.priority);
+//   }
+// }
+
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
   enqueue(val, priority) {
-    this.values.push({ val, priority });
-    this.sort();
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
   }
   dequeue() {
-    return this.values.shift();
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
   }
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
   }
 }
 
