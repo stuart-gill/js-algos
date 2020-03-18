5da8720dab61833e8fc542e0e6a09398a54270b0
Stuart Gill <stuart.a.gill@gmail.com>
2020-03-18 15:09:31 -0700

    finished

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -10,5 +10,17 @@ const memoizedFibonacci = (n, memo = []) => {
   return memo[n];
 };
 
-console.log(recursiveFibonacci(45));
+console.log(recursiveFibonacci(10));
 console.log(memoizedFibonacci(45));
+
+// tabulation is like memoization but building up from the bottom rather than top down.
+// also not recursive
+const tabulatedFibonacci = n => {
+  let fibSeq = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2];
+  }
+  return fibSeq[n];
+};
+
+console.log(tabulatedFibonacci(45));
