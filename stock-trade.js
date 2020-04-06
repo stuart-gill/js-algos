18055c442c74f11688f77ad736e52baf668db803
Stuart Gill <stuart.a.gill@gmail.com>
2020-04-06 13:32:56 -0700

    remove lows array, simplify and only keep current low

--- stock-trade.js
+++ stock-trade.js
@@ -1,20 +1,18 @@
 // Write an efficient function that takes stockPrices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
 // Stock prices are listed in array in chronological order
+// trades must occur-- if stock drops all day, find lowest possible loss
 
-const stockPrices = [32, 10, 7, 5, 8, 11, 9, 27, 2];
+const stockPrices = [32, 10, 7, 5, 2, 45];
 
 function getMaxProfit(prices) {
-  let lows = [prices[0]];
   let currentLow = prices[0];
   let currentBest = prices[1] - prices[0];
   for (let i = 1; i < prices.length; i++) {
     if (prices[i] < currentLow) currentLow = prices[i];
-    lows.push(currentLow);
-    if (prices[i] - lows[i - 1] > currentBest) {
-      currentBest = prices[i] - lows[i - 1];
+    if (prices[i] - currentLow > currentBest) {
+      currentBest = prices[i] - currentLow;
     }
   }
-  console.log(`lows: ${lows}`);
   console.log(`current best: ${currentBest}`);
   return currentBest;
 }
