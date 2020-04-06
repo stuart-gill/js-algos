74722be8b3e9b1123e7cd53c990d499f07992bc5
Stuart Gill <stuart.a.gill@gmail.com>
2020-04-06 13:49:11 -0700

    clean up and add comments

--- stock-trade.js
+++ stock-trade.js
@@ -6,9 +6,12 @@ const stockPrices = [32, 10, 7, 5, 2, 45];
 
 function getMaxProfit(prices) {
   let currentLow = prices[0];
+  // initialize current best to be difference between first and second prices, even if that's negative
   let currentBest = prices[1] - prices[0];
   for (let i = 1; i < prices.length; i++) {
+    // reset current low if current element is less
     if (prices[i] < currentLow) currentLow = prices[i];
+    // check diff between current price and running low price, and set it to currentBest if this difference is higher
     if (prices[i] - currentLow > currentBest) {
       currentBest = prices[i] - currentLow;
     }
@@ -19,3 +22,5 @@ function getMaxProfit(prices) {
 
 getMaxProfit(stockPrices);
 // Returns 6 (buying for $5 and selling for $11)
+
+// I initially set this one up to build arrays storing running highest and lowest amounts, but wasn't necessary given that you're only looking for the max intra-day delta
