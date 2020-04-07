cd5c1f1c554c45903deb5f317a0406d330aa8a3e
Stuart Gill <stuart.a.gill@gmail.com>
2020-04-06 20:28:09 -0700

    reduce complexity of merge meetings, add comments

--- cake/merge-meetings.js
+++ cake/merge-meetings.js
@@ -29,32 +29,46 @@ function mergeRanges(allMeetings) {
     let added = false;
     for (let mergedMeeting of merged) {
       if (
+        //meeting overlaps end of merged meeting
         meeting.startTime > mergedMeeting.startTime &&
         meeting.startTime <= mergedMeeting.endTime &&
         meeting.endTime > mergedMeeting.endTime
       ) {
         mergedMeeting.endTime = meeting.endTime;
         added = true;
+        break;
       } else if (
+        //meeting overlaps beginning of merged meeting
         meeting.startTime < mergedMeeting.startTime &&
         meeting.endTime >= mergedMeeting.startTime &&
         meeting.endTime < mergedMeeting.endTime
       ) {
         mergedMeeting.startTime = meeting.startTime;
         added = true;
+        break;
       } else if (
+        //meeting overlaps entirety of merged meeting
         meeting.startTime < mergedMeeting.startTime &&
         meeting.endTime > mergedMeeting.endTime
       ) {
         mergedMeeting.startTime = meeting.startTime;
         mergedMeeting.endTime = meeting.endTime;
         added = true;
+        break;
+      } else if (
+        //meeting is subsumed my merged meeting
+        meeting.startTime > mergedMeeting.startTime &&
+        meeting.endTime < mergedMeeting.endTime
+      ) {
+        added = true;
+        break;
       }
-      console.log(merged);
     }
     if (added === false) {
+      //otherwise, add the meeting to merged array
       merged.push(meeting);
     }
+    console.log(merged);
   }
 
   return merged;
